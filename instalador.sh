7239c3fdf185522480e0f8c6221126c4db135f9a
yullidias <yulli.dias@hotmail.com>
2017-09-21 15:33:02 -0300

    adicionando a função que instala os recursos para utilizar o latex

---
+++ instalador.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+function latex(){
+# Instalar distribuição tex, normas abnt e texstudio respectivamente
+	sudo apt-get install texlive-full abntex texstudio
+}
+
+echo "runnig..."
+
+
