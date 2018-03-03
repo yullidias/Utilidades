fbf4d868a5f64d5ea638a1d13d64bfd9a44ec744
yullidias <yullidias@hotmail.com>
2018-03-03 13:40:33 -0300

    adicionando opção para instar o Virtual Box

--- instalador.sh
+++ instalador.sh
@@ -5,8 +5,15 @@ function Latex(){
 	sudo apt-get install --yes texlive-full abntex texstudio aspell
 }
 
+function VirtualBoxAMD64(){
+	wget https://download.virtualbox.org/virtualbox/5.2.8/virtualbox-5.2_5.2.8-121009~Debian~jessie_amd64.deb
+	sudo dpkg -i virtualbox*.deb
+	sudo apt-get install linux-headers-amd64 linux-headers-3.16.0-4-amd64
+	sudo /sbin/vboxconfig
+}
 
-instalar=$(zenity --list --checklist --column "instalar" --column "Programas" FALSE Latex)
+instalar=$(zenity --list --checklist --column "instalar" --column "Programas" FALSE Latex FALSE VirtualBoxAMD64
+)
 IFS="|"
 programas="$instalar"
 for p in $programas; do
